bf999a7d062b72150257b67ae35f4cf6e9bfed61
dam1iesmh <dam1iesmh@mail.com>
2017-11-29 20:04:52 +0100

    ClaseDAM1.java INICIAL

--- ManejoRepositorios/src/ClaseDAM1.java
+++ ManejoRepositorios/src/ClaseDAM1.java
@@ -1,9 +1,42 @@
 
 public class ClaseDAM1 {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Cada estudiante añadirá su nombre
+		String profesor = "Luis de Blas";
+
+		String al01_SergioAmor = "";
+		String al02_JorgeArroy = "";
+		String al03_ManuelBoo = "";
+		String al04_JoseDaBuenaga = "";
+		String al05_DavidCicero = "";
+		String al06_NinaCimadevilla = "";
+		String al07_DanielDehesa = "";
+		String al09_JonatanDelRio = "";
+		String al11_VictorDiaz = "";
+		String al12_VictorFernandez = "";
+		String al13_MarioFernandez = "";
+		String al16_JonatanBordetas = "";
+		String al17_AnaGonzalez = "";
+		String al18_IkerIriondo = "";
+		String al19_DavidMartinez = "";
+		String al20_AdrianNoriega = "";
+		String al21_DiegoPerez = "";
+		String al22_RubenPerez = "";
+		String al23_MarioPerez = "";
+		String al24_ChristianPinera= "";
+		String al25_AlvaroPolanco = "";
+		String al26_ElenaPuig = "";
+		String al27_JesusRuiz = "";
+		String al28_PabloSanchez= "";
+		String al30_MarcosSuarez = "";
+		String al31_DiegoVelez = "";
+		String al32_MarcosGonzalez= "";
+
 
 	}
 
 }
+
+
